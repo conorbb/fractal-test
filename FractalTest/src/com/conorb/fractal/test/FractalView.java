d2e5c04bd3dc51c878b8006b14503ba756a6438a
Conor <conorb@gmail.com>
2013-04-23 17:20:51 +0200

    Apache 2

--- FractalTest/src/com/conorb/fractal/test/FractalView.java
+++ FractalTest/src/com/conorb/fractal/test/FractalView.java
@@ -1,6 +1,25 @@
 /*
+ * Copyright 2012 Conor Byrne.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
- * STRORING IN STACKED INT *
+ * Based on code by Josef Jelinek
+ * http://java.rubikscube.info/
+ */
+
+/*
+ * 
+ * STORING IN STACKED INT *
  * (alpha << 24) | (red << 16) | (green << 8) | blue
  *
  * RETREIVE FROM STACKED INT (FOR COLOR)
